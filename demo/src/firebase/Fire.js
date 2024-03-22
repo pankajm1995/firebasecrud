fc5188f5cca16e10f14416bd79c1bb447efd90aa
Pankaj Kumar Maurya <153347011+pankajm1995@users.noreply.github.com>
2024-03-22 06:26:39 +0000

    d

--- demo/src/firebase/Fire.js
+++ demo/src/firebase/Fire.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 import{database} from './config'
-import {addDoc, collection, getDocs} from 'firebase/firestore'
+import './table.css'
+import {addDoc, collection, doc, getDocs} from 'firebase/firestore'
 function Fire()
 {
     const [fname,setFname]= useState('');
     const [lname,setLname]=useState('');
     const [val,setValue]= useState([]);
-
+    const[show,setShow]=useState('');
     const value = collection(database,"demo")
     const handlecreate=async()=>
     {
         await addDoc(value,{name1:fname,name2:lname})
     }
 
+    const handleDelete=async(id)=>
+    {
+        const deleteval=doc()
+    }
     useEffect(()=>
     {
         const getdata= async()=>
@@ -23,6 +28,8 @@ function Fire()
         }
         getdata();
     })
+
+
     return(
         <div>
             <h1>Fire base component</h1>
@@ -32,18 +39,28 @@ function Fire()
             <h2>Last Name
                 <input value={lname} onChange={(e)=>setLname(e.target.value)} style={{marginRight:'8px'}}></input>
             </h2>
-            <button onClick={handlecreate}>Add data</button>
+            {!show?<button onClick={handlecreate}>Add data</button>:
+            <button onClick={handleupdate}>Updatedata</button>}
             {
                 val.map(value=><div>
-                    <table style={{border:'1px solid black'}}>
-                    <thead>
-                        <td>
-                            <tr>
-                            <h1>{value.name1}</h1>
+                    <table>
+                            <thead>
+                                <tr>
+                                <td>First Name</td>
+                                <td>Last Name</td>
+                                <td>Action</td>
+                                </tr>
+                            </thead>  
+                            <td>
+                                <h1>{value.name1}</h1>    
+                            </td> 
+                            <td>
                             <h1>{value.name2}</h1>
-                            </tr>
-                        </td>
-                    </thead>
+                            </td>
+                            <td>
+                                <button onClick={handleDelete(value.id)}>Delete</button>
+                                <button onClick={handleEdit(value.id,value.name1,value.name2)}>Edit</button>
+                            </td>
                     </table>
                 </div>)
             }
